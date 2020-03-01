7ee32d5ebf273450464667cdb296fbc4eb53a4b8
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-03-01 20:31:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const $table = document.createElement('Table');
 let number = 0;
 
+function getTable(){//eslint-disable-line
 for (let i = 0; i < 10; i++) {//eslint-disable-line
-    const $tr = document.createElement('tr');
-    $table.appendChild($tr);
+        const $tr = document.createElement('tr');
+        $table.appendChild($tr);
 
     for (let j = 0; j < 10; j++) {//eslint-disable-line
-        const $td = document.createElement('td');
-        $tr.appendChild($td);
-        number++;
-        $td.textContent = number;
+            const $td = document.createElement('td');
+            $tr.appendChild($td);
+            number++;
+            $td.textContent = number;
+        }
     }
+    return $table.outerHTML;
 }
 
-document.body.outerHTML += $table.outerHTML;
+document.body.outerHTML = getTable($table);
